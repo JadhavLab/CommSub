6ab26e64bb0c9c8ef466a4e2390021a9ff1b824f
Ryan <ryoung@brandeis.edu>
2023-06-17 02:20:31 -0400

    refamilairizing ... and adding doc files here and there and cleaning. hit a bug on the timeVarying function.

--- META_SCRIPTS/TheScript.m
+++ META_SCRIPTS/TheScript.m
@@ -22,9 +22,18 @@ disp(Option);
 
 %%%%%%%%%%%%%%%% OBTAIN EVENT MATRICES    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [Events] = events.ThetaDeltaRipple(Option);
+% Documentation
+% Events is a struct with fields:
+% - .times : array of times of events
+% - .H     : Event Matrix, T x 3, and each column are theta, delta, ripple
+% - .Hvals : Event Matrix, T x 3, values without nans
+% - .Hnanlocs : Event Matrix, T x 3, logicals of nans
 
 %%%%%%%%%%%%%%%% CUT WINDOWS WITH EVENT MATRICES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 [cellOfWindows, cutoffs] = windows.ThetaDeltaRipple(Events, Option);
+% Documentation
+% -  cellOfWindows: 1 x nPatterns cell array of windows
+% -  cutoffs: nPatterns x 1 vector of cutoffs
 
 %%%%%%%%%%%%%%%% ACQUIRE SPIKES %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Getting spikes
@@ -34,30 +43,23 @@ disp(Option);
 
 % filter the neurons whose firing rate is lower than specified threshold
 if Option.preProcess_FilterLowFR 
+    disp("Filtering low FR neurons")
     [spikeCountMatrix, spikeRateMatrix, avgFR, areaPerNeuron, cell_index]...
         = trialSpikes.filterFR(spikeCountMatrix, spikeRateMatrix, 0.1, ...
         timeBinStartEnd, areaPerNeuron, cell_index);
 end
 
+
 %%%%%%%%%%%%%%%% ACQUIRE TRIALS FROM WINDOWS + SPIKES %%%%%%%%%%%%%%%%%%%
 [spikeSampleMatrix, spikeSampleTensor, trialTimes] = trialSpikes.generate(...
     spikeCountMatrix, timeBinMidPoints, cellOfWindows, ... % RYAN bug here .. timeBinStartEnd instead of timeBinMidPoints
-    Option.timesPerTrial, nPatternAndControl);
+    Option.timesPerTrial, Option.nPatternAndControl);
 
 
 % %%%%%%%%%%%%%%% SETUP RAW DATA STRUCTURE %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Structure for separated data
 r.hpc = struct;
 r.pfc = struct;
-r.avgFR = avgFR;
-r.areaPerNeuron = areaPerNeuron;
-r.pfc.nNeurons = nPFCneurons;
-r.hpc.nNeurons = nHPCneurons;
-r.windowInfo.cellOfWindows = cellOfWindows;
-r.windowInfo.nWindows      = cellfun(@(x) size(x, 1), cellOfWindows);
-r.nPattern = 3;
-r.nControl = nPatternAndControl - r.nPattern;
-
 %%%%%%%%%%%%%%%% SEPRATE BRAIN AREA DATASULT STRUCTURES %%%%%%%%%%%%%%%%%%
 %% Separate spikesSampleMatrix/Tensor by area that neurons are in PFC and neurons that in HPC
 [r.pfc.FR, r.hpc.FR] = trialSpikes.separateFiringRate(avgFR, areaPerNeuron);
@@ -65,23 +67,32 @@ r.pfc.T = trialSpikes.separateSpikes(spikeSampleTensor, areaPerNeuron, "PFC");
 r.hpc.T = trialSpikes.separateSpikes(spikeSampleTensor, areaPerNeuron, "CA1");
 r.pfc.X = trialSpikes.separateSpikes(spikeSampleMatrix, areaPerNeuron, "PFC");
 r.hpc.X = trialSpikes.separateSpikes(spikeSampleMatrix, areaPerNeuron, "CA1");
-
 %% Separate firing pattern into source and target
 [nPFCneurons,~,~] = size(r.pfc.X{1});
 [nHPCneurons,~,~] = size(r.hpc.X{1});
-
 r.celllookup = cellInfo.getCellIdentities(Option.animal, cell_index,...
                                           areaPerNeuron);
+r.avgFR         = avgFR;
+r.areaPerNeuron = areaPerNeuron;
+r.pfc.nNeurons = nPFCneurons;
+r.hpc.nNeurons = nHPCneurons;
+r.windowInfo.cellOfWindows = cellOfWindows;
+r.windowInfo.nWindows      = cellfun(@(x) size(x, 1), cellOfWindows);
+r.nPattern = 3;
+r.nControl = Option.nPatternAndControl - r.nPattern;
+
 
 %%%%%%%%%%%%%%%% SETUP PARTITIONS AND RESULT STRUCTURES %%%%%%%%%%%%%%%%%%
 Patterns = trialSpikes.partitionAndInitialize(r, Option);
 
-%%%%%%%%%%%%%%%% ANANLYSIS SECTION    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%% ANALYSIS SECTION    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 if Option.analysis.rankRegress
+    % TODO: fix Option.rankregress => Option.rankRegress
     Patterns = analysis.rankRegress(Patterns, Option);
 end
 
 if Option.analysis.timeVarying
+    % ISSUE: hits a bug on line 4
     Components = analysis.timeVarying(Patterns, Option);
 end
 
