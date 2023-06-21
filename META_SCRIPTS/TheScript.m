b40d6cc72ea7c5c911327c0b462ca7d45b85d367
Ryan <ryoung@brandeis.edu>
2023-06-21 18:16:35 -0400

    decluttering the raw/processed plots -- moving to a call (like our other analysis sections) ...

--- META_SCRIPTS/TheScript.m
+++ META_SCRIPTS/TheScript.m
@@ -93,25 +93,7 @@ Patterns = trialSpikes.partitionAndInitialize(r, Option);
 
 % Plots regarding the raw and processed data
 if Option.analysis.checks
-    disp("Running checks")
-    % firing rate checks
-    a = @() plots.frChecks(r, "appendFigTitle", char(Option.animal));
-    b = @() plots.event.plotEventDetails(Events, Option, ...
-        "savePath", fullfile(figuredefine(), "eventDetails"), ...
-        "appendFigTitle", char(Option.animal), ...
-        'r', r, ...
-        'spikePlotStyle', 'heatmapBehind');
-    c = @() plots.event.plotSingleEvents(cellOfWindows, Events, Option, r, ...
-        'before', 0.5, 'after', 0.5, 'appendFigTitle', char(Option.animal), ...
-        'eventTypes', ["theta", "delta", "ripple"], ...
-        'displayType', 'heatmap');
-    % Run plots in parallel
-    % a = parfeval(gcp, a, 0);
-    % b = parfeval(gcp, b, 0);
-    % c = parfeval(gcp, c, 0);
-    % Wait for plots to finish
-    % wait(a); wait(b); wait(c);
-    a(); b(); c();
+    plots.runChecks(Patterns, Option, r, cellOfWindows);
 end
 
 % Rank regression of network pattern windows of spiking activity
