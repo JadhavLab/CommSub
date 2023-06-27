b9936658d64acf62cce7b2a2c8b7b4f247234561
Ryan <ryoung@brandeis.edu>
2023-06-26 23:44:57 -0400

    addition of preprocess option (gauss, zscore). documentation.

--- packages/+option/defaults.m
+++ packages/+option/defaults.m
@@ -3,7 +3,9 @@ function Default = defaults(name)
 %
 % Returns the default structure encoding some basic options
 %
-% Now changes in default values automatically occur across scripts
+% This script causes default values to change across scripts calling it
+%
+% 
 %
 % Important variables we iterate over:
 %   - Default.generateH = "fromCoherence "+" fromRipTimes" |
@@ -44,46 +46,54 @@ Default.equalWindowsAcrossPatterns    = true;            % whether all three pat
 Default.quantileToMakeWindows         = 0.85;
 
 Default.thetadelta_outlierQuantile    = [0, 0.995];     % quantile to remove outliers from theta and delta
-% TRIAL WINDOW
+
+% ---- TRIAL WINDOW -----
 % how long -- this will interpolate all to be timesPerTrial long
 Default.spikeShiftSize                = 0.010; % used to default Default.timesPerTrial - size of actual trial sample
 Default.timesPerTrial                 = ceil(range(Default.winSize)/Default.spikeShiftSize); % 
 assert(Default.timesPerTrial > 1);
 
 % About brain areas
-Default.sourceArea                    = "CA1";           % only when there are
-Default.waysOfPartitions              = 2;
+Default.sourceArea                    = "CA1"; % only when there are
+Default.waysOfPartitions              = 2;     
 
-% Controls
-Default.singleControl                 = false;           % whether to use just one control column
-Default.oldControlBehavior            = false;           % whether to call genratePatternShuffle for control (Which creates shuffled Hc. if false, Hc=H -- which needs to be true for finding low periods of activity)
-Default.lowerControl                  = true;
+% ---  Controls ---- 
+Default.singleControl                 = false; % whether to use just one control column
+Default.oldControlBehavior            = false; % whether to call genratePatternShuffle for control (Which creates shuffled Hc. if false, Hc=H -- which needs to be true for finding low periods of activity)
+Default.lowerControl                  = true;  % whether to use the lower control (Hc) or the upper control (Hc2)
+
+% ----- Preprocessing -----
 Default.binsToMatchFR                 = 20;
-Default.preProcess_FilterLowFR        = true;
-Default.preProcess_matchingDiscreteFR = true;
+Default.preProcess_FilterLowFR        = true;  % whether to filter out low FR cells
+Default.preProcess_matchingDiscreteFR = true;  % whether to match discrete FR
+Default.preProcess_gaussianFilter     = 5;     % gaussian that covers this many samples
+Default.preProcess_zscore             = true;  % whether to zscore the data
 
+% ----- Data resampling -----
 % Data resamlping
-Default.numPartition                  = 50;              % ways to split source and target
+Default.numPartition                  = 50; % subsamples of target/source split
 
 % Time-varying
-Default.dimCompAnalysis               = 5;
-Default.stablePerf                    = 0.9;
+Default.dimCompAnalysis               = 5; % number of components to use in dimensionality reduction
+Default.stablePerf                    = 0.9; % performance threshold for stable periods
 
+% ----- Analysis -----
 % What to run?
 Default.generateH                       = join(["fromSpectra","fromRipTimes"], "  ");
-Default.analysis.run_selected_genH      = false;
-Default.analysis.checks                 = true;
-Default.analysis.rankRegress            = true;
+Default.analysis.run_selected_genH      = false; % 📜 this
+Default.analysis.checks                 = true;  % whether to run checks: FR, singleEvents, H
+Default.analysis.rankRegress            = true;  % rank regression
 Default.analysis.factorAnalysis         = false; % took too long -- tend to not run this
-Default.analysis.singleNeuronPrediction = false;
+Default.analysis.singleNeuronPrediction = false; % single neuron prediction in RRR
 Default.analysis.timeVarying            = false; % rewrite
 Default.analysis.cca                    = false; % kim 2022
-Default.analysis.JPECC                  = false; %
+Default.analysis.JPECC                  = false; % joint peri-event cannonical correlation
 Default.analysis.reverse                = false; % commsubspace high -> network patterns
 % Default.generateH = "fromWpli " + " fromRipTimes";
 % Default.generateH = "fromCoherence "+" fromRipTimes";
 % Default.generateH = "fromSpectra "+" fromRipTimes";
-%
+
+% ---- Analysis parameters -----
 Default.rankRegress.cvnum = 10; % number of cross-validation folds
 Default.jpecc.cvnum = 4; % number of cross-validation folds
 
@@ -92,6 +102,7 @@ Default.saveRaw = false; % whether to save the raw data
 
 
 %% --------- OPTIONS SPECIFIC TO SCRIPT TYPE ---------------------------------
+%  one could place options that are specific to a script type here
 if     name == "TheScript"
 elseif name == "RRR_dPCA"
 elseif name == "MethodTest"
