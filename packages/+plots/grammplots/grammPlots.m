fa5cf09db2147694e13671c32a255f2a668e4972
Ryan <ryoung@brandeis.edu>
2023-07-14 13:10:05 -0400

    removePred works, but plots look a little crazy

--- packages/+plots/grammplots/grammPlots.m
+++ packages/+plots/grammplots/grammPlots.m
@@ -1,5 +1,13 @@
 %%
-T = query.getPatternTable(Patterns_AllAnimals);
+if ~exist('T', 'var')
+    T = query.getPatternTable(Patterns_AllAnimals);
+end
+% Cast any int types to double for table T
+for i = 1:width(T)
+    if isinteger(T.(i))
+        T.(i) = double(T.(i));
+    end
+end
 
 patternType = T.patternType;
 patternType( ~contains(patternType, '-' ), : ) = patternType( ~contains(patternType, '-' ), : ) + "-";
@@ -16,12 +24,13 @@ fig('hpc-pfc, hilbert vs coherence')
 clear g
 %T.winSize = cell2mat(T.winSize);
 %T = sortrows(T, [16, 21]);
-filteredsubset = T.generateH == 'fromFilteredEEG  fromRipTimes'...
-              & T.directionality == "hpc-pfc";
+filteredsubset = T.generateH == 'fromSpectra  fromRipTimes'... % NOTE: 2023 ry change to fromSpectra
+                    & T.directionality == "hpc-pfc";
 coherencesubset = T.generateH == 'fromWpli  fromRipTimes'  ...
-              & T.directionality == "hpc-pfc" ;
+                    & T.directionality == "hpc-pfc" ;
 filteredsubset = T(filteredsubset,:);
 coherencesubset = T(coherencesubset,:);
+assert(~isempty(filteredsubset))
 
 x = filteredsubset.percMax_rrDim;
 y = coherencesubset.percMax_rrDim;
@@ -50,12 +59,11 @@ g.set_names('x', "Hilbert" + newline + "Predictive Dims", ...
 x = filteredsubset.full_model_performance;
 y = coherencesubset.full_model_performance;
 %subset = filteredsubset.directionality == "hpc-pfc" ;
-
-corner_kws = {'edges',-0.15:0.01:0.15, 'aspect',0.6, 'location', [0.5], 'fill', 'transparent', 'normalization',  'countdensity'}; %WARNING : EDGES OF HISTOGRAM ARE SET MANUALLY! you need this to surround your data
+corner_kws = {'edges',-0.16:0.01:0.16, 'aspect',0.6, 'location', [0.5], 'fill', 'transparent', 'normalization',  'countdensity'}; %WARNING : EDGES OF HISTOGRAM ARE SET MANUALLY! you need this to surround your data
 g(1,2) = gramm( ... 'subset', subset,...
             'x', x,...
             'y', y,...
-            'color', categorical(filteredsubset.patternAbstract),...
+            'color',     categorical(filteredsubset.patternAbstract),...
             'lightness', categorical(filteredsubset.control));
 % assert(all(hpcsubset.patternType == pfcsubset.patternAbstract))
 g(1,2).facet_grid(categorical(filteredsubset.control), categorical(filteredsubset.patternAbstract))
@@ -64,20 +72,17 @@ g(1,2).geom_abline('style','k:');
 g(1,2).stat_cornerhist(corner_kws{:});
 g(1,2).set_point_options('base_size', point_size);
 g(1,2).set_text_options('label_scaling', 1.2, 'base_size', 14);
-
 g(1,2).set_names('x', "Hilbert" + newline + "Model Performance", ...
     'y', "Coh" + newline + "Model Performance", ...
     'Color', 'Pattern', ...
     'Lightness', 'Treatment/Control')
-
 g.draw();
 
 
 %% Average pattern maxDimPerc single direction, all patterns
-clf
 figure(5)
+clf
 subset =   T.directionality == "hpc-pfc";
-
 hpcsubset = T.directionality == 'hpc-hpc';
 pfcsubset = T.directionality == 'hpc-pfc';
 
